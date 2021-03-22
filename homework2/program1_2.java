9f9cad21f32532a30c8793517eefdd19020fd7fc
1-wxf <71303471+1-wxf@users.noreply.github.com>
2021-03-22 15:52:50 +0800

    Add files via upload

---
+++ homework2/program1_2.java
@@ -0,0 +1,9 @@
+package homework2;
+public class program1_2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Programming is fun!");
+		System.out.println("Fundamentals First");
+		System.out.println("Problem Driven");
+	}
+}
